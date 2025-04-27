35e77506eec8d1bf5b3372575e50f5d2bf65af59
Jeromel Pushparaj <jeromal03@gmail.com>
2025-04-27 22:33:54 +0530

    fix parenthesis

--- app/CodeBase/htdocs/_templates/_head.php
+++ app/CodeBase/htdocs/_templates/_head.php
@@ -40,7 +40,7 @@
 	<?php if(file_exists($_SERVER['DOCUMENT_ROOT']."/css/".basename($_SERVER['PHP_SELF'], '.php').".css")) {?>
 	<link href="/css/<?= basename($_SERVER['PHP_SELF'], '.php');?>.css" rel="stylesheet">
 
-	<?}?>
+	<?php } ?>
 
 
 
