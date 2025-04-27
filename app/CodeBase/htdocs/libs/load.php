8aa7a007c24195485f30219fd8c73173ceab9720
Jeromel Pushparaj <jeromal03@gmail.com>
2025-04-27 22:32:29 +0530

    include files fix

--- app/CodeBase/htdocs/libs/load.php
+++ app/CodeBase/htdocs/libs/load.php
@@ -6,8 +6,6 @@ include_once 'includes/Database.class.php';
 include_once 'includes/UserSession.class.php';
 include_once 'includes/WebAPI.class.php';
 include_once 'app/Post.class.php';
-include_once 'includes/Device.class.php';
-include_once 'includes/ApiKey.class.php';
 
 global $__site_config;
 /*
